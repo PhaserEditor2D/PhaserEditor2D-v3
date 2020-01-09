ca4f54f2d05feafb9ae492ee80133285dbb68c21
Arian Fornaris <developers@phasereditor2d.com>
2020-01-09 10:06:28 -0500

    Scene Editor: fixes data created by New Scene and Prefab wizards.

--- source/client/plugins/phasereditor2d.scene/ui/dialogs/NewPrefabFileDialogExtension.ts
+++ source/client/plugins/phasereditor2d.scene/ui/dialogs/NewPrefabFileDialogExtension.ts
@@ -2,21 +2,27 @@ namespace phasereditor2d.scene.ui.dialogs {
 
     export class NewPrefabFileDialogExtension extends files.ui.dialogs.NewFileContentExtension {
 
+        private static createSceneData(): core.json.SceneData {
+            return {
+                id: Phaser.Utils.String.UUID(),
+                settings: {},
+                sceneType: core.json.SceneType.PREFAB,
+                displayList: [],
+                meta: {
+                    app: "Phaser Editor 2D - Scene Editor",
+                    url: "https://phasereditor2d.com",
+                    contentType: scene.core.CONTENT_TYPE_SCENE
+                }
+            };
+        }
+
         constructor() {
             super({
                 dialogName: "Prefab File",
                 dialogIcon: ScenePlugin.getInstance().getIcon(ICON_GROUP),
                 fileExtension: "scene",
                 initialFileName: "Prefab",
-                fileContent: JSON.stringify({
-                    sceneType: core.json.SceneType.PREFAB,
-                    displayList: [],
-                    meta: {
-                        app: "Phaser Editor 2D - Scene Editor",
-                        url: "https://phasereditor2d.com",
-                        contentType: scene.core.CONTENT_TYPE_SCENE
-                    }
-                } as core.json.SceneData)
+                fileContent: JSON.stringify(NewPrefabFileDialogExtension.createSceneData())
             });
         }
 
--- source/client/plugins/phasereditor2d.scene/ui/dialogs/NewSceneFileDialogExtension.ts
+++ source/client/plugins/phasereditor2d.scene/ui/dialogs/NewSceneFileDialogExtension.ts
@@ -2,21 +2,27 @@ namespace phasereditor2d.scene.ui.dialogs {
 
     export class NewSceneFileDialogExtension extends files.ui.dialogs.NewFileContentExtension {
 
+        private static createSceneData(): core.json.SceneData {
+            return {
+                id: Phaser.Utils.String.UUID(),
+                settings: {},
+                sceneType: core.json.SceneType.SCENE,
+                displayList: [],
+                meta: {
+                    app: "Phaser Editor 2D - Scene Editor",
+                    url: "https://phasereditor2d.com",
+                    contentType: scene.core.CONTENT_TYPE_SCENE
+                }
+            };
+        }
+
         constructor() {
             super({
                 dialogName: "Scene File",
                 dialogIcon: ScenePlugin.getInstance().getIcon(ICON_GROUP),
                 fileExtension: "scene",
                 initialFileName: "Scene",
-                fileContent: JSON.stringify({
-                    sceneType: core.json.SceneType.SCENE,
-                    displayList: [],
-                    meta: {
-                        app: "Phaser Editor 2D - Scene Editor",
-                        url: "https://phasereditor2d.com",
-                        contentType: scene.core.CONTENT_TYPE_SCENE
-                    }
-                } as core.json.SceneData)
+                fileContent: JSON.stringify(NewSceneFileDialogExtension.createSceneData())
             });
         }
 
