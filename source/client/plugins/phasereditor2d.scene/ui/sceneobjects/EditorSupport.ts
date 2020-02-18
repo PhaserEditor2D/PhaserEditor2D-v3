e1ba985194d60e8d883004aa75df0e9086f02f9a
Arian Fornaris <developers@phasereditor2d.com>
2020-02-17 23:40:39 -0500

    Scene Editor: fixes screen bounds computation with different flip values.

--- source/client/plugins/phasereditor2d.scene/ui/sceneobjects/EditorSupport.ts
+++ source/client/plugins/phasereditor2d.scene/ui/sceneobjects/EditorSupport.ts
@@ -181,26 +181,18 @@ namespace phasereditor2d.scene.ui.sceneobjects {
                 h = h / sprite.scaleY;
             }
 
-            let flipX = sprite.flipX ? -1 : 1;
-            let flipY = sprite.flipY ? -1 : 1;
-
-            if (sprite instanceof Phaser.GameObjects.TileSprite) {
-                flipX = 1;
-                flipY = 1;
-            }
-
             const ox = sprite.originX;
             const oy = sprite.originY;
 
-            const x = -w * ox * flipX;
-            const y = -h * oy * flipY;
+            const x = -w * ox;
+            const y = -h * oy;
 
             const tx = sprite.getWorldTransformMatrix();
 
             tx.transformPoint(x, y, points[0]);
-            tx.transformPoint(x + w * flipX, y, points[1]);
-            tx.transformPoint(x + w * flipX, y + h * flipY, points[2]);
-            tx.transformPoint(x, y + h * flipY, points[3]);
+            tx.transformPoint(x + w, y, points[1]);
+            tx.transformPoint(x + w, y + h, points[2]);
+            tx.transformPoint(x, y + h, points[3]);
 
             return points.map(p => camera.getScreenPoint(p.x, p.y));
         }
