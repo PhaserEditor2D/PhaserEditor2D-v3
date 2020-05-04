312dd4a818bc557fc33452a011e23471b6bf0663
Arian Fornaris <developers@phasereditor2d.com>
2020-05-04 15:27:55 -0400

    Update templates in get-latest-phaser script.

--- scripts/get-latest-phaser.sh
+++ scripts/get-latest-phaser.sh
@@ -19,4 +19,14 @@ rm -R $PHASER_PLUGIN/src/phaser/matter.ts
 cp $PHASER_TYPES/phaser.d.ts  $PHASER_PLUGIN/src/phaser/phaser.ts
 cp $PHASER_TYPES/matter.d.ts  $PHASER_PLUGIN/src/phaser/matter.ts
 
+# templates
+
+rm -f source/templates/include/lib/*
+rm -f source/templates/include/types/*
+
+cp $PHASER_DIST/phaser.js source/templates/include/lib/
+cp $PHASER_TYPES/phaser.d.ts  source/templates/include/types/
+cp $PHASER_TYPES/matter.d.ts  source/templates/include/types/
+
+
 
