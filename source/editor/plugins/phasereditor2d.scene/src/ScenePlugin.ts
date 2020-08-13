f776611b595fee19e7acbe7384c0356885434c4a
PhaserEditor2D <developers@phasereditor2d.com>
2020-08-12 21:03:43 -0400

    #46: Scene Editor: the Pan scene tool.

--- source/editor/plugins/phasereditor2d.scene/src/ScenePlugin.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ScenePlugin.ts
@@ -195,8 +195,9 @@ namespace phasereditor2d.scene {
                 new ui.sceneobjects.RotateTool(),
                 new ui.sceneobjects.ScaleTool(),
                 new ui.sceneobjects.OriginTool(),
+                new ui.sceneobjects.TileSpriteSizeTool(),
                 new ui.sceneobjects.SelectionRegionTool(),
-                new ui.sceneobjects.TileSpriteSizeTool()
+                new ui.sceneobjects.PanTool(),
             ));
         }
 
