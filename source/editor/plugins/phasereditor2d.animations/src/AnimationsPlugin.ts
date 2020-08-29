e0941766b728e52858d98c1f8e60202b77849f1f
PhaserEditor2D <developers@phasereditor2d.com>
2020-08-28 22:33:25 -0400

    Animations editor: select all.

--- source/editor/plugins/phasereditor2d.animations/src/AnimationsPlugin.ts
+++ source/editor/plugins/phasereditor2d.animations/src/AnimationsPlugin.ts
@@ -42,9 +42,17 @@ namespace phasereditor2d.animations {
 
             reg.addExtension(new colibri.ui.ide.commands.CommandExtension(manager => {
 
+                // escape
+
                 manager.addHandlerHelper(colibri.ui.ide.actions.CMD_ESCAPE,
                     args => args.activePart instanceof ui.editors.AnimationsEditor,
                     args => args.activeEditor.setSelection([]));
+
+                // select all
+
+                manager.addHandlerHelper(colibri.ui.ide.actions.CMD_SELECT_ALL,
+                    args => args.activePart instanceof ui.editors.AnimationsEditor,
+                    args => (args.activePart as ui.editors.AnimationsEditor).selectAll())
             }));
         }
     }
