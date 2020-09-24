df5eb0089c4ed3df0063ae7ee90420a2df4ee12d
PhaserEditor2D <developers@phasereditor2d.com>
2020-09-24 15:43:34 -0400

    Scene Editor: more about scene blocks.

--- source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksCellRendererProvider.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksCellRendererProvider.ts
@@ -23,6 +23,10 @@ namespace phasereditor2d.scene.ui.blocks {
             } else if (element === sceneobjects.ObjectList) {
 
                 return new controls.viewers.IconImageCellRenderer(ScenePlugin.getInstance().getIcon(ICON_LIST));
+
+            } else if (typeof(element) === "string" && BLOCKS_SECTIONS.indexOf(element) >= 0) {
+
+                return new controls.viewers.IconImageCellRenderer(colibri.ColibriPlugin.getInstance().getIcon(colibri.ICON_FOLDER));
             }
 
             return super.getCellRenderer(element);
--- source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksTreeRendererProvider.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksTreeRendererProvider.ts
@@ -7,20 +7,22 @@ namespace phasereditor2d.scene.ui.blocks {
 
     export const PREFAB_SECTION = "Prefab";
 
+    export const BLOCKS_SECTIONS = [
+
+        BUILTIN_SECTION,
+        PREFAB_SECTION,
+        pack.core.IMAGE_TYPE,
+        pack.core.ATLAS_TYPE,
+        pack.core.SPRITESHEET_TYPE,
+        pack.core.BITMAP_FONT_TYPE
+    ];
+
     export class SceneEditorBlocksTreeRendererProvider extends pack.ui.viewers.AssetPackTreeViewerRenderer {
 
         constructor(viewer: controls.viewers.TreeViewer) {
             super(viewer, false);
 
-            this.setSections([
-
-                BUILTIN_SECTION,
-                PREFAB_SECTION,
-                pack.core.IMAGE_TYPE,
-                pack.core.ATLAS_TYPE,
-                pack.core.SPRITESHEET_TYPE,
-                pack.core.BITMAP_FONT_TYPE
-            ]);
+            this.setSections(BLOCKS_SECTIONS);
         }
 
         isSectionSorted(section: string) {
