03ae98aabc4108eead3a17305685555fb627c33c
PhaserEditor2D <developers@phasereditor2d.com>
2020-10-03 13:39:02 -0400

    Tilemap section.

--- source/editor/plugins/phasereditor2d.scene/src/ScenePlugin.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ScenePlugin.ts
@@ -203,7 +203,8 @@ namespace phasereditor2d.scene {
                 page => new ui.sceneobjects.TextContentSection(page),
                 page => new ui.sceneobjects.TextSection(page),
                 page => new ui.sceneobjects.BitmapTextSection(page),
-                page => new ui.sceneobjects.ListSection(page)
+                page => new ui.sceneobjects.ListSection(page),
+                page => new ui.sceneobjects.TilemapSection(page)
             ));
 
             // scene tools
