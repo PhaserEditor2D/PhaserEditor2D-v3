64bc12a196a32c72ef5c8c6b051da71d5fe6bfd5
PhaserEditor2D <developers@phasereditor2d.com>
2020-10-07 22:06:03 -0400

    TilemapLayer code generator.

--- source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/tilemap/TilemapLayerCodeDOMBuilder.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/tilemap/TilemapLayerCodeDOMBuilder.ts
@@ -6,19 +6,30 @@ namespace phasereditor2d.scene.ui.sceneobjects {
 
         buildCreateObjectWithFactoryCodeDOM(args: IBuildObjectFactoryCodeDOMArgs): core.code.MethodCallCodeDOM {
 
-            return new code.MethodCallCodeDOM("createStaticLayer");
+            const tilemapLayer = args.obj as TilemapLayer;
+
+            const tilemap = tilemapLayer.tilemap as Tilemap;
+
+            const tilemapVarName = code.formatToValidVarName(tilemap.getEditorSupport().getLabel());
+
+            const call = new code.MethodCallCodeDOM("createStaticLayer", tilemapVarName);
+
+            call.argLiteral(tilemapLayer.layer.name);
+            call.arg(tilemapVarName + ".tilesets");
+
+            return call;
         }
 
         buildCreatePrefabInstanceCodeDOM(args: IBuildPrefabConstructorCodeDOMArgs): void {
-
+            throw new Error("Not supported");
         }
 
         buildPrefabConstructorDeclarationCodeDOM(args: IBuildPrefabConstructorDeclarationCodeDOM): void {
-
+            throw new Error("Not supported");
         }
 
         buildPrefabConstructorDeclarationSupperCallCodeDOM(args: IBuildPrefabConstructorDeclarationSupperCallCodeDOMArgs): void {
-
+            throw new Error("Not supported");
         }
     }
 }
