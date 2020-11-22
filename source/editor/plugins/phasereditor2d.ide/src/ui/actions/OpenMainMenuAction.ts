e19e1fece3e5463bb613ab8fc20877d1a2b9e27e
PhaserEditor2D <developers@phasereditor2d.com>
2020-11-22 18:01:13 -0500

    Unlock dialog.

--- source/editor/plugins/phasereditor2d.ide/src/ui/actions/OpenMainMenuAction.ts
+++ source/editor/plugins/phasereditor2d.ide/src/ui/actions/OpenMainMenuAction.ts
@@ -40,6 +40,16 @@ namespace phasereditor2d.ide.ui.actions {
 
             menu.addCommand(colibri.ui.ide.actions.CMD_SHOW_COMMAND_PALETTE);
 
+            if (IDEPlugin.getInstance().isDesktopMode()) {
+
+                menu.add(new controls.Action({
+                    text: "Unlock Phaser Editor 2D",
+                    callback: () => {
+                        new dialogs.UnlockDialog().create();
+                    }
+                }));
+            }
+
             menu.add(new controls.Action({
                 text: "Help",
                 callback: () => controls.Controls.openUrlInNewPage("https://help.phasereditor2d.com")
