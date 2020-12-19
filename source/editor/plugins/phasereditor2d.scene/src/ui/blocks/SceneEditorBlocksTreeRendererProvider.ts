627ec61cc958d2501a11f1ec40d2447732efa340
PhaserEditor2D <developers@phasereditor2d.com>
2020-12-19 09:30:04 -0500

    Viewers: fix text metric for different devicePixelRatio.

--- source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksTreeRendererProvider.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/blocks/SceneEditorBlocksTreeRendererProvider.ts
@@ -44,20 +44,5 @@ namespace phasereditor2d.scene.ui.blocks {
 
             return true;
         }
-
-        prepareContextForText(args: controls.viewers.RenderCellArgs) {
-
-            super.prepareContextForText(args);
-
-            if (args.obj instanceof io.FilePath) {
-
-                const type = colibri.Platform.getWorkbench().getContentTypeRegistry().getCachedContentType(args.obj);
-
-                if (type === core.CONTENT_TYPE_SCENE) {
-
-                    args.canvasContext.font = `italic ${controls.FONT_HEIGHT}px ${controls.FONT_FAMILY}`;
-                }
-            }
-        }
     }
 }
