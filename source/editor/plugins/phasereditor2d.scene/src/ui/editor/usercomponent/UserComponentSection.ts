046a0a38e0accee61972ec5b9d4ff937e5daac7c
PhaserEditor2D <developers@phasereditor2d.com>
2021-03-25 16:28:35 -0400

    User components: options button for the component's Game Object Type & Super Class.

--- source/editor/plugins/phasereditor2d.scene/src/ui/editor/usercomponent/UserComponentSection.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/editor/usercomponent/UserComponentSection.ts
@@ -20,7 +20,8 @@ namespace phasereditor2d.scene.ui.editor.usercomponent {
 
         createForm(parent: HTMLDivElement) {
 
-            const comp = this.createGridElement(parent, 2);
+            const comp = this.createGridElement(parent, 3);
+            comp.style.gridTemplateColumns = "auto 1fr auto";
 
             {
                 // Name
@@ -35,9 +36,9 @@ namespace phasereditor2d.scene.ui.editor.usercomponent {
             }
 
             this.stringProp(comp, "GameObjectType", "Game Object Type",
-                "Name of the type of the Game Object that this component can be added on.");
+                "Name of the type of the Game Object that this component can be added on.", () => this.createGameObjectTypeOptions());
 
-            this.stringProp(comp, "BaseClass", "Super Class", "Name of the super class of the component. It is optional.");
+            this.stringProp(comp, "BaseClass", "Super Class", "Name of the super class of the component. It is optional.", () => this.createSuperClassOptions());
 
             this.booleanProp(comp,
                 "ListenStart", "Listen Start Event", "Listen the `start()` event. Requires `UserComponent` as super-class.");
@@ -50,7 +51,37 @@ namespace phasereditor2d.scene.ui.editor.usercomponent {
                 "Listen the `destroy()` event. Fired when the game object is destroyed. Requires `UserComponent` as super-class.");
         }
 
-        private stringProp(comp: HTMLElement, prop: string, propName: string, propHelp: string) {
+        private createSuperClassOptions(): string[] {
+
+            const options = new Set(ScenePlugin.getInstance().getSceneFinder()
+                .getUserComponentsModels()
+                .flatMap(model => model.model.getComponents())
+                .map(comp => comp.getBaseClass())
+                .filter(name => name !== undefined && name !== null && name.trim().length > 0));
+
+            options.delete("UserComponent");
+
+            return ["UserComponent", ...([...options].sort())];
+        }
+
+        private createGameObjectTypeOptions(): string[] {
+
+            const options = new Set(ScenePlugin.getInstance().getSceneFinder()
+                .getUserComponentsModels()
+                .flatMap(model => model.model.getComponents())
+                .map(comp => comp.getGameObjectType()));
+
+            for (const option of ScenePlugin.getInstance()
+                .getGameObjectExtensions()
+                .map(e => e.getPhaserTypeName())) {
+
+                options.add(option);
+            }
+
+            return [...options].sort();
+        }
+
+        private stringProp(comp: HTMLElement, prop: string, propName: string, propHelp: string, options?: () => string[]) {
 
             this.createLabel(comp, propName, propHelp);
 
@@ -73,12 +104,50 @@ namespace phasereditor2d.scene.ui.editor.usercomponent {
                     this.getSelection().map(c => c["get" + prop]()));
             });
 
+            if (options) {
+
+                const btn = new controls.IconControl(colibri.ColibriPlugin.getInstance().getIcon(colibri.ICON_CONTROL_TREE_COLLAPSE), true);
+                btn.getCanvas().style.alignSelf = "center";
+                comp.appendChild(btn.getCanvas());
+
+                btn.getCanvas().addEventListener("click", e => {
+
+                    const menu = new controls.Menu();
+
+                    for (const option of options()) {
+
+                        menu.addAction({
+                            text: option,
+                            callback: () => {
+
+                                text.value = option;
+
+                                this.getEditor().runOperation(() => {
+
+                                    for (const comp1 of this.getSelection()) {
+
+                                        comp1["set" + prop](text.value);
+                                    }
+                                });
+                            }
+                        });
+                    }
+
+                    menu.createWithEvent(e, true);
+                });
+
+            } else {
+
+                text.style.gridColumn = "2 / span 2";
+            }
+
             return text;
         }
 
         private booleanProp(comp: HTMLElement, prop: string, propName: string, propHelp: string) {
 
             const checkbox = this.createCheckbox(comp, this.createLabel(comp, propName, propHelp));
+            checkbox.style.gridColumn = "2 / span 2";
 
             checkbox.addEventListener("change", e => {
 
