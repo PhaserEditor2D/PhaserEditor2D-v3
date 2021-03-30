156963d43659657a0998117fbf0af0b7c08d07bb
PhaserEditor2D <developers@phasereditor2d.com>
2021-03-30 13:21:12 -0400

    Webpack project templates.

---
+++ source/templates/providers/Phaser Editor 2D/Basic Webpack TypeScript/files/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./src/index.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name]-[contenthash].bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, "./dist"),
+        open: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/index.html"),
+            minify: false
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "static",
+                    globOptions: {
+                        ignore: ["**/publicroot"]
+                    }
+                }
+            ]
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
