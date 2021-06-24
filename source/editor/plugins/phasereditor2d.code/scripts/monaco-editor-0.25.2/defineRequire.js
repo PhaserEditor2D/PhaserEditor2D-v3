ecf67a6188c66c567caeba2a3594c037b8cc2e03
PhaserEditor2D <developers@phasereditor2d.com>
2021-06-24 02:05:39 -0400

    Fixes monaco `require` path.

--- source/editor/plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/defineRequire.js
+++ source/editor/plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/defineRequire.js
@@ -1,1 +1,1 @@
-var require = { paths: { 'vs': 'app/plugins/phasereditor2d.code/scripts/monaco-editor-0.20.0/min/vs' } };
+var require = { paths: { 'vs': 'app/plugins/phasereditor2d.code/scripts/monaco-editor-0.25.2/min/vs' } };
