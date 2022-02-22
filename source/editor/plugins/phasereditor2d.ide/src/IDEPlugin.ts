89632b969a673193a96fec25fc82da0413aaa9bb
PhaserEditor2D <developers@phasereditor2d.com>
2022-02-22 16:59:26 -0500

    Set beta version

--- source/editor/plugins/phasereditor2d.ide/src/IDEPlugin.ts
+++ source/editor/plugins/phasereditor2d.ide/src/IDEPlugin.ts
@@ -248,7 +248,7 @@ namespace phasereditor2d.ide {
 
     /* program entry point */
 
-    export const VER = "3.32.1-alpha.1";
+    export const VER = "3.32.1-beta.1";
 
     async function main() {
 
