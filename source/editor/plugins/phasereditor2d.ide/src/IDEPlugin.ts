d605e2c1a4e54682b18fa8cc42dc731e1818c03c
PhaserEditor2D <developers@phasereditor2d.com>
2022-04-11 01:16:53 -0400

    Update version.

--- source/editor/plugins/phasereditor2d.ide/src/IDEPlugin.ts
+++ source/editor/plugins/phasereditor2d.ide/src/IDEPlugin.ts
@@ -248,7 +248,7 @@ namespace phasereditor2d.ide {
 
     /* program entry point */
 
-    export const VER = "3.33.0-alpha.1";
+    export const VER = "3.33.0-alpha.2";
 
     async function main() {
 
