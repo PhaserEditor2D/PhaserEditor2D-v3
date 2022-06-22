9b9411d61e34101ad74282f7a8cfa2485546e3af
PhaserEditor2D <developers@phasereditor2d.com>
2022-06-22 15:06:44 -0400

    Polygon: generates setTo code for nested prefabs.

--- source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/polygon/PolygonComponent.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/polygon/PolygonComponent.ts
@@ -11,26 +11,16 @@ namespace phasereditor2d.scene.ui.sceneobjects {
 
         buildSetObjectPropertiesCodeDOM(args: ISetObjectPropertiesCodeDOMArgs) {
 
-            // TODO!!!!!! Missing to implement the setTo(..) method.
-            // check this issue: https://github.com/photonstorm/phaser/issues/6151
+            const obj = this.getObject();
+            const support = obj.getEditorSupport();
 
-            // const obj = this.getObject();
-            // const support = obj.getEditorSupport();
+            if (support.isNestedPrefabInstance()
+                && support.isUnlockedProperty(PolygonComponent.points)) {
 
-            // if (support.isNestedPrefabInstance() &&
-            //     (support.isUnlockedPropertyXY(TriangleComponent.p1)
-            //         || support.isUnlockedPropertyXY(TriangleComponent.p2)
-            //         || support.isUnlockedPropertyXY(TriangleComponent.p3))) {
-
-            //     const dom = new core.code.MethodCallCodeDOM("setTo", args.objectVarName);
-            //     dom.argFloat(obj.x1);
-            //     dom.argFloat(obj.y1);
-            //     dom.argFloat(obj.x2);
-            //     dom.argFloat(obj.y2);
-            //     dom.argFloat(obj.x3);
-            //     dom.argFloat(obj.y3);
-            //     args.statements.push(dom);
-            // }
+                const dom = new core.code.MethodCallCodeDOM("setTo", args.objectVarName);
+                dom.argLiteral(obj.points);
+                args.statements.push(dom);
+            }
         }
     }
 }
