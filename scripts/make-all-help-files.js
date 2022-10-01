e7e68c5b728c74223523e90a0e347027f463ac5a
PhaserEditor2D <developers@phasereditor2d.com>
2022-09-30 23:20:13 -0400

    Fixes missing polygon help.

--- scripts/make-all-help-files.js
+++ scripts/make-all-help-files.js
@@ -204,13 +204,14 @@ utils.makeHelpFile([
     "Phaser.GameObjects.Ellipse.smoothness",
 
     "Phaser.GameObjects.Triangle",
-
     "Phaser.Geom.Triangle.x1",
     "Phaser.Geom.Triangle.y1",
     "Phaser.Geom.Triangle.x2",
     "Phaser.Geom.Triangle.y2",
     "Phaser.Geom.Triangle.x3",
     "Phaser.Geom.Triangle.y3",
+
+    "Phaser.GameObjects.Polygon",
 
     "Phaser.GameObjects.Layer",
 
--- source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/polygon/PolygonComponent.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/polygon/PolygonComponent.ts
@@ -2,7 +2,7 @@ namespace phasereditor2d.scene.ui.sceneobjects {
 
     export class PolygonComponent extends Component<Polygon> {
 
-        static points = SimpleProperty("points", Polygon.DEFAULT_POINTS, "Points", "phaser:Phaser.Geom.Polygon.points");
+        static points = SimpleProperty("points", Polygon.DEFAULT_POINTS, "Points", "The polygon's points, in a string format `X1 Y1 Y2 X2...`");
 
         constructor(obj: Polygon) {
             super(obj,
