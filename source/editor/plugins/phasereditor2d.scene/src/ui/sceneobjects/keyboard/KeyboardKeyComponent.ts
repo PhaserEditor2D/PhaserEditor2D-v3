308e950ddd040b5eac0818e79844ed701ab0dd95
PhaserEditor2D <developers@phasereditor2d.com>
2023-04-18 21:57:11 -0400

    Fixes keyboard key objects serialization.

--- source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/keyboard/KeyboardKeyComponent.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/keyboard/KeyboardKeyComponent.ts
@@ -1,9 +1,9 @@
 namespace phasereditor2d.scene.ui.sceneobjects {
 
-    export class KeyboardKeyComponent extends   PlainObjectComponent<KeyboardKey>  {
+    export class KeyboardKeyComponent extends PlainObjectComponent<KeyboardKey>  {
 
         static keyCode = SimpleProperty("keyCode", "SPACE", "Key Code", "The keycode of this key.", true);
-        
+
         constructor(obj: KeyboardKey) {
             super(obj, KeyboardKeyComponent.keyCode);
         }
