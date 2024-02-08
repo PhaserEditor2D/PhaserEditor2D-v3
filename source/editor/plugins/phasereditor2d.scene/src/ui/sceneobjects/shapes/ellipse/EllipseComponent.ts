35ee1bc6be158a3557301d16d45c44f9c9c63c2a
PhaserEditor2D <developers@phasereditor2d.com>
2024-02-08 09:19:33 -0500

    Adds increment info to Ellipse component.

--- source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/ellipse/EllipseComponent.ts
+++ source/editor/plugins/phasereditor2d.scene/src/ui/sceneobjects/shapes/ellipse/EllipseComponent.ts
@@ -2,8 +2,7 @@ namespace phasereditor2d.scene.ui.sceneobjects {
 
     export class EllipseComponent extends Component<Ellipse> {
 
-        static smoothness = SimpleProperty("smoothness", 64, "Smoothness", "phaser:Phaser.GameObjects.Ellipse.smoothness");
-
+        static smoothness = SimpleProperty("smoothness", 64, "Smoothness", "phaser:Phaser.GameObjects.Ellipse.smoothness", false, undefined, 1, 0);
 
         constructor(obj: Ellipse) {
             super(obj, [
